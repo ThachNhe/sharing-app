69383fa8067468035ac3cd0716e4338aea7d360f
thachdv <thachdv.it.dev@gmail.com>
2025-07-27 23:12:21 +0700

    initial client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -31,7 +31,24 @@ const Home = () => {
     }
   };
 
-  return <div className=""></div>;
+  return (
+    <div className="min-h-screen relative">
+      {/* Background - Split into two sections */}
+      <div className="absolute inset-0">
+        {/* Top half - Image/Pattern background */}
+        <div className="h-1/2 bg-gradient-to-br from-blue-100 via-purple-50 to-pink-100 relative overflow-hidden">
+          {/* Decorative elements */}
+          <div className="absolute top-10 left-10 w-20 h-20 bg-blue-200 rounded-full opacity-60"></div>
+          <div className="absolute top-32 right-20 w-16 h-16 bg-purple-200 rounded-full opacity-40"></div>
+          <div className="absolute bottom-10 left-1/4 w-12 h-12 bg-pink-200 rounded-full opacity-50"></div>
+          <div className="absolute top-20 left-1/3 w-8 h-8 bg-yellow-200 rounded-full opacity-70"></div>
+        </div>
+
+        {/* Bottom half - Purple gradient */}
+        <div className="h-1/2 bg-gradient-to-br from-purple-500 via-purple-600 to-purple-700"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
